81e44df4d2a8671e23a0c310cee5cdab81ee7df6
wandergithub <wandergonzalez0@hotmail.com>
2022-08-31 15:06:11 -0400

    Set store

---
+++ src/redux/config.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import reducer from "./ecommerce/ecommerce";
+
+const middlewares = []
+
+const store = configureStore({
+  reducer,
+  middleware: middlewares,
+});
+
+export default store;
---
+++ src/redux/ecommerce/ecommerce.js
@@ -0,0 +1,14 @@
+import ecommerceAppState from "./state";
+//Actions
+
+// Reducer
+
+const reducer = (state = ecommerceAppState, action = {}) => {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+// Action Creators
+
+export default reducer;
