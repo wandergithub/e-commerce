a7ed209ca7c3b543e35d2d98d5a378c5891c9206
wandergithub <wandergonzalez0@hotmail.com>
2022-08-31 16:54:39 -0400

    Add css style to complete layout

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,31 +1,30 @@
 import { useSelector } from 'react-redux';
 import { Button, Container, Typography } from '@mui/material';
+import '../style/article.css';
 
 const Article = () => {
   const state = useSelector((state) => state);
   const game = state.games[0];
   return (
-    <div>
+    <div className="main-container">
       <div className="a1-container">
+        <img src="https://i.pinimg.com/550x/36/eb/6b/36eb6bd7b0adaefd1357a464d78d2142.jpg" alt="Game cover" />
         <Container>
-          <Typography variant="h2" gutterButtom>{game.name}</Typography>
-          <Typography variant="body1" gutterButtom>{game.description}</Typography>
-          <Button variant="contained">Buy now</Button>
-        </Container>
-        <Container>
-          <img src="https://i.pinimg.com/550x/36/eb/6b/36eb6bd7b0adaefd1357a464d78d2142.jpg" alt="Game cover" />
+          <Typography variant="h2" className="mb-3" gutterBottom>{game.name}</Typography>
+          <Typography variant="body1" className="p-description" gutterBottom>{game.description}</Typography>
+          <Button variant="contained" className="mt-3">Buy now</Button>
         </Container>
       </div>
       <div className="a2-container">
         <div>
-          <Typography variant="h5" gutterButtom>More details</Typography>
-          <Typography variant="body1" gutterButtom>
+          <Typography variant="h5" className="mb-3" gutterBottom>More details</Typography>
+          <Typography variant="body1" gutterBottom>
             {game.description}
           </Typography>
         </div>
         <div>
-          <Typography variant="h5" gutterButtom>Reviews</Typography>
-          <Typography variant="body1" gutterButtom>
+          <Typography variant="h5" className="mb-3" gutterBottom>Reviews</Typography>
+          <Typography variant="body1" gutterBottom>
             {game.description}
           </Typography>
         </div>
