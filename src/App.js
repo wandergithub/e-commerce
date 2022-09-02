4aa77229531a3f5be367ee917ed6224687baff61
wandergithub <wandergonzalez0@hotmail.com>
2022-09-02 15:13:05 -0400

    Add action, modified reducer to fetch products from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchCategories } from './redux/ecommerce/ecommerce';
+import { fetchInitialData } from './redux/ecommerce/ecommerce';
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchCategories);
+    dispatch(fetchInitialData());
   }, []);
 
   return (
-    <h1>Hello</h1>
+    <h1>Hello e commerce app</h1>
   );
 }
 
--- src/redux/ecommerce/ecommerce.js
+++ src/redux/ecommerce/ecommerce.js
@@ -1,23 +1,29 @@
 import axios from 'axios';
 // Actions
 const CATEGORIES_FETCHED = 'my-app/ecommerce/CATEGORIES_FETCHED';
+const PRODUCTS_FETCHED = 'my-app/ecommerce/PRODUCTS_FETCHED';
 
 // Reducer
 const reducer = (state = {}, action = {}) => {
   switch (action.type) {
     case CATEGORIES_FETCHED:
       return { ...state, categories: action.payload };
+    case PRODUCTS_FETCHED:
+      return { ...state, products: action.payload };
     default: return state;
   }
 };
 
 // Action Creators
 export const loadCategories = (categories) => ({ type: CATEGORIES_FETCHED, payload: categories });
+export const loadProducts = (products) => ({ type: PRODUCTS_FETCHED, payload: products });
 
 // Thunks
-export const fetchCategories = () => async (dispatch) => {
-  const response = await axios.get('https://api.escuelajs.co/api/v1/categories');
-  dispatch(loadCategories(response.data));
+export const fetchInitialData = () => async (dispatch) => {
+  const categoriesResponse = await axios.get('https://api.escuelajs.co/api/v1/categories');
+  dispatch(loadCategories(categoriesResponse.data));
+  const productsResponse = await axios.get('https://api.escuelajs.co/api/v1/products');
+  dispatch(loadProducts(productsResponse.data));
 };
 
 export default reducer;
