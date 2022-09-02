bd6b689efbd877b875a8a7c38a3c7a4faa064d4c
wandergithub <wandergonzalez0@hotmail.com>
2022-09-02 12:18:54 -0400

    add hook to dispatch fetching actions on page load

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 import Article from './components/Article';
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react';
+import { fetchCategories } from './redux/ecommerce/ecommerce';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() =>{
+    dispatch(fetchCategories);
+  }, []);
+
   return (
     <Article />
   );
