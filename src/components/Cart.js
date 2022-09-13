10d1c81f478969c50e2bc1f9e675f70dd81158eb
wandergithub <wandergonzalez0@hotmail.com>
2022-09-13 11:24:25 -0400

    Refactor code

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,22 +1,13 @@
 import { useState, forwardRef } from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import ListItemText from '@mui/material/ListItemText';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Fab from '@mui/material/Fab';
-import ShopIcon from '@mui/icons-material/Shop';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import ShoppingCartOutlined from '@mui/icons-material/ShoppingCartOutlined';
-import Avatar from '@mui/material/Avatar';
-import CloseIcon from '@mui/icons-material/Close';
-import Slide from '@mui/material/Slide';
+import {
+  Button, Dialog, ListItemText, ListItem,
+  ListItemAvatar, List, Divider, AppBar,
+  Toolbar, IconButton, Typography, Fab, Avatar, Slide,
+} from '@mui/material';
+import {
+  Shop as ShopIcon, DeleteForever as DeleteForeverIcon, ShoppingCartOutlined, Close as CloseIcon,
+} from '@mui/icons-material';
+
 import PropTypes from 'prop-types';
 /* eslint-disable react/jsx-props-no-spreading */
 const Transition = forwardRef((props, ref) => <Slide direction="up" ref={ref} {...props} />);
--- src/redux/config.js
+++ src/redux/config.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import ecommerceReducer from './ecommerce/ecommerce';
 
 const store = configureStore({
   reducer: ecommerceReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
