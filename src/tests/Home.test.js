f6409eb886bc25231cdfdd3ba4a9b63afdecf51f
wandergithub <wandergonzalez0@hotmail.com>
2023-02-20 15:17:06 -0400

    create Home component test: categorie cards link 'see items takes the user to items page

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -2,12 +2,11 @@ import { Provider } from "react-redux";
 import Home from "../components/Home";
 import store from "./store_config";
 import { BrowserRouter } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-describe("Categories page /root", () => {
+describe("Home component", () => {
   test("loads and display categories", async () => {
-    // Arrange
     render(
       <Provider store={store}>
         <BrowserRouter>
@@ -20,5 +19,21 @@ describe("Categories page /root", () => {
 
     // Assert
     expect(screen.getByText("categorie 1")).toBeInTheDocument();
+  });
+
+  test("SEE ITEMS link takes you to items page when clicked", () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </Provider>
+    );
+    const link = screen.getByText("See items");
+    
+    expect(window.location.pathname).toBe("/");
+    fireEvent.click(link);
+
+    expect(window.location.pathname).toBe("/items");
   });
 });
