f81841cb33cfa1036b5b38baaf6f6155188ad3c8
wandergithub <wandergonzalez0@hotmail.com>
2023-02-20 15:03:52 -0400

    add store mock for tests

---
+++ src/tests/store_config.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import ecommerceReducer from "../redux/ecommerce/ecommerce";
+
+const store = configureStore({
+  reducer: ecommerceReducer,
+  preloadedState: {
+    categories: [
+      {
+        name: "categorie 1",
+        image: "image sample 1",
+      },
+    ],
+  },
+});
+
+export default store;
