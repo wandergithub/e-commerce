ea59850faefea2049912d4a15c5452c8f39963ea
wandergithub <wandergonzalez0@hotmail.com>
2023-02-24 10:57:13 -0400

    Add info notification to DesktopBar to non functional features

--- src/components/DesktopBar.js
+++ src/components/DesktopBar.js
@@ -1,6 +1,5 @@
 import {
-  Badge,
-  IconButton, InputBase, Paper,
+  Badge, IconButton, InputBase, Paper,
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import LanguageIcon from '@mui/icons-material/Language';
@@ -8,12 +7,41 @@ import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
+import {
+  NotificationContainer,
+  NotificationManager,
+} from 'react-notifications';
 import Cart from './Cart';
 import logo from '../assets/logos/logo.png';
-
+import 'react-notifications/lib/notifications.css';
 
 const DesktopBar = (props) => {
   const { cart } = props;
+  // Notifications
+  const createNotification = (type) => () => {
+    switch (type) {
+      case 'info':
+        NotificationManager.info('This is a demo version, we are working on this feature right now.');
+        break;
+      case 'success':
+        NotificationManager.success('Success message', 'Title here');
+        break;
+      case 'warning':
+        NotificationManager.warning(
+          'Warning message',
+          'Close after 3000ms',
+          3000,
+        );
+        break;
+      case 'error':
+        NotificationManager.error('Error message', 'Click me!', 5000, () => {
+          alert('callback');
+        });
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <div className="option-bar">
       <div className="logo">
@@ -22,12 +50,17 @@ const DesktopBar = (props) => {
         </Link>
       </div>
       <div className="country-input">
-        <IconButton type="button" sx={{ px: '10px' }} aria-label="search">
+        <IconButton
+          type="button"
+          sx={{ px: '10px' }}
+          aria-label="search"
+          onClick={createNotification('info')}
+        >
           <AddLocationAltIcon color="primary" />
         </IconButton>
       </div>
       <div className="search-bar">
-        <Paper>
+        <Paper onClick={createNotification('info')}>
           <InputBase
             sx={{ ml: 1, flex: 1 }}
             placeholder="Search Item"
@@ -39,13 +72,23 @@ const DesktopBar = (props) => {
         </Paper>
       </div>
       <div className="language">
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <IconButton
+          type="button"
+          sx={{ p: '10px' }}
+          aria-label="search"
+          onClick={createNotification('info')}
+        >
           <LanguageIcon color="primary" />
         </IconButton>
       </div>
       <div className="log-menu">
         {/* Menu desplegable sign in, log in */}
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <IconButton
+          type="button"
+          sx={{ p: '10px' }}
+          aria-label="search"
+          onClick={createNotification('info')}
+        >
           <AccountCircle color="primary" />
         </IconButton>
       </div>
@@ -54,6 +97,7 @@ const DesktopBar = (props) => {
           <Cart items={cart} />
         </Badge>
       </div>
+      <NotificationContainer />
     </div>
   );
 };
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,33 +1,33 @@
-import { render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import App from "../App";
-import store from "./store_config";
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
+import App from '../App';
+import store from './store_config';
 
-describe("App integration test", () => {
+describe('App integration test', () => {
   beforeEach(() => {
     render(
       <Provider store={store}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </Provider>
+      </Provider>,
     );
   });
 
-  test("it renders on / root page", () => {
-    expect(window.location.pathname).toBe("/");
+  test('it renders on / root page', () => {
+    expect(window.location.pathname).toBe('/');
   });
 
-  test("renders correctly", () => {
+  test('renders correctly', () => {
     const tree = renderer
       .create(
         <Provider store={store}>
           <BrowserRouter>
             <App />
           </BrowserRouter>
-        </Provider>
+        </Provider>,
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
