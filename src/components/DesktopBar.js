bbba8ab8fbdf14467e3bd8d30834f51862a48f68
Lz-01 <lennart.zinn@web.de>
2023-02-26 11:02:30 +0100

    undo changes in Desktop.js

--- src/components/DesktopBar.js
+++ src/components/DesktopBar.js
@@ -1,6 +1,5 @@
 import {
-  Badge,
-  IconButton, InputBase, Paper,
+    Badge, IconButton, InputBase, Paper,
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import LanguageIcon from '@mui/icons-material/Language';
@@ -14,78 +13,81 @@ import 'react-notifications/lib/notifications.css';
 import createNotification from './shared/Notifications';
 
 const DesktopBar = (props) => {
-  const { cart } = props;
-  return (
-    <div className="option-bar">
-        <IconButton aria-label="delete">
-            <div className="logo">
+    const { cart } = props;
+
+    return (
+        <div className="option-bar">
+            <IconButton aria-label="delete">
                 <Link to="e-commerce/" className="link">
                     <img className="e-com" src={logo} alt="e commerce logo" />
                 </Link>
-            </div>
-        </IconButton>
-    <div className="country-input">
-        <IconButton
-            type="button"
-            sx={{ px: '10px' }}
-            aria-label="search"
-            onClick={createNotification('info')}
-            disabled
-        >
-            <AddLocationAltIcon color="primary." />
-            <IconButton type="button" sx={{ px: '10px' }} aria-label="search">
-                <AddLocationAltIcon color="primary" />
             </IconButton>
-        </IconButton>
-    </div>
-    <div className="search-bar">
-        <Paper onClick={createNotification('info')}>
-            <Paper>
-                <InputBase
-                    sx={{ ml: 1, flex: 1 }}
-                    placeholder="Search Item"
-                    inputProps={{ 'aria-label': 'search item' }}
-                />
+            <div className="country-input">
+                <IconButton
+                    type="button"
+                    sx={{ px: '10px' }}
+                    aria-label="search"
+                    onClick={createNotification('info')}
+                    disabled
+                >
+                    <AddLocationAltIcon color="primary." />
+                </IconButton>
+            </div>
+            <div className="search-bar">
+                <Paper onClick={createNotification('info')}>
+                    <InputBase
+                        sx={{ ml: 1, flex: 1 }}
+                        placeholder="Search Item"
+                        inputProps={{ 'aria-label': 'search item' }}
+                    />
+                    <IconButton
+                        type="button"
+                        sx={{ p: '10px' }}
+                        aria-label="search"
+                        disabled
+                    >
+                        <SearchIcon color="primary." />
+                    </IconButton>
+                </Paper>
+            </div>
+            <div className="language">
                 <IconButton
                     type="button"
                     sx={{ p: '10px' }}
                     aria-label="search"
+                    onClick={createNotification('info')}
                     disabled
                 >
-                    <SearchIcon color="primary." />
-                    <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                        <SearchIcon color="primary" />
-                    </IconButton>
+                    <LanguageIcon color="primary." />
                 </IconButton>
-            </Paper></Paper>
-    </div>
-    <div className="language">
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-          <LanguageIcon color="primary" />
-        </IconButton>
-      </div>
-      <div className="log-menu">
-        {/* Menu desplegable sign in, log in */}
-        <Link to="profile" className="link">
-          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-            <AccountCircle color="primary" />
-          </IconButton>
-        </Link>
-      </div>
-      <div className="cart">
-        <Badge badgeContent={cart.length} color="primary">
-          <Cart items={cart} />
-        </Badge>
-      </div>
-    </div>
-  );
+            </div>
+            <div className="log-menu">
+                <Link to="profile" className="link">
+                <IconButton
+                    type="button"
+                    sx={{ p: '10px' }}
+                    aria-label="search"
+                    onClick={createNotification('info')}
+                    disabled
+                >
+                    <AccountCircle color="primary.darker" />
+                </IconButton>
+                </Link>
+            </div>
+            <div className="cart">
+                <Badge badgeContent={cart.length} color="primary">
+                    <Cart items={cart} />
+                </Badge>
+            </div>
+        </div>
+    );
 };
 
 DesktopBar.propTypes = {
-  cart: PropTypes.instanceOf(Array),
+    cart: PropTypes.instanceOf(Array),
 };
 
 DesktopBar.defaultProps = {
-  cart: [],
+    cart: [],
 };
 export default DesktopBar;
