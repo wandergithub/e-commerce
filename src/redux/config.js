d4129b6ed846b6fff4192d05ecc8741b0276039f
wandergithub <wandergonzalez0@hotmail.com>
2023-03-07 21:11:00 -0400

    Add logger middleware on development enviroment to see state changes

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import ecommerceReducer from './ecommerce/ecommerce';
 
 const store = configureStore({
   reducer: ecommerceReducer,
+  middleware: (getDefaultMiddleware) => (process.env.NODE_ENV === 'development' ? getDefaultMiddleware().concat(logger) : getDefaultMiddleware()),
 });
 
 export default store;
