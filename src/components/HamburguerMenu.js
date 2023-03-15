05ad1dc302d3c0cb9f778330b946e295f65998e0
wandergithub <wandergonzalez0@hotmail.com>
2023-03-15 13:40:22 -0400

    Add new nav button to mobile version

--- src/components/HamburguerMenu.js
+++ src/components/HamburguerMenu.js
@@ -1,69 +1,110 @@
-import { useState, forwardRef } from 'react';
-import {
-  Button, Dialog,
-  AppBar,
-  Toolbar, IconButton, Typography, Fab, Slide,
-} from '@mui/material';
-import {
-  Shop as ShopIcon, Close as CloseIcon,
-} from '@mui/icons-material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { styled, alpha } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import Divider from '@mui/material/Divider';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { useState } from 'react';
 
-const Transition = forwardRef((props, ref) => (
-  /* eslint-disable react/jsx-props-no-spreading */
-  <Slide direction="right" ref={ref} {...props} />
-));
-Transition.displayName = 'Transition';
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    /* eslint-disable react/jsx-props-no-spreading */
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light'
+        ? 'rgb(55, 65, 81)'
+        : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
 
-const HamburguerMenu = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
+export default function HamburguerMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
-
   const handleClose = () => {
-    setOpen(false);
+    setAnchorEl(null);
   };
 
   return (
     <div>
-      <Button variant="text" onClick={handleClickOpen}>
-        <MenuIcon color="primary" />
-
+      <Button
+        id="demo-customized-button"
+        aria-controls={open ? 'demo-customized-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+      >
+        Options
       </Button>
-      <Dialog
-        fullScreen
-        fullWidth
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
+        }}
+        anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        TransitionComponent={Transition}
       >
-        <AppBar sx={{ position: 'relative' }}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={handleClose}
-              aria-label="close"
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography sx={{ ml: 2, flex: 1 }} variant="h5" component="h1">
-              Shopping cart
-            </Typography>
-            <Fab variant="extended" color="success">
-              <ShopIcon sx={{ mr: 1 }} />
-              Complete Buy
-            </Fab>
-            <Button autoFocus color="inherit" onClick={handleClose}>
-              Continue Shopping
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Dialog>
+        <MenuItem onClick={handleClose} disableRipple>
+          <EditIcon />
+          Edit
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <FileCopyIcon />
+          Duplicate
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem onClick={handleClose} disableRipple>
+          <ArchiveIcon />
+          Archive
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <MoreHorizIcon />
+          More
+        </MenuItem>
+      </StyledMenu>
     </div>
   );
-};
-
-export default HamburguerMenu;
+}
